876aa06f9b09d2bb907d5b77efd93c4344b3810f
nicole pank <nicole.pank@gmail.com>
2013-09-19 10:00:10 -0500

    Add User and Comparer classes with initialize methods.

--- lib/follower_clash.rb
+++ lib/follower_clash.rb
@@ -1,3 +1,26 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'twitter'
+
+module FollowerClash
+
+  class User
+    attr_reader :login
+
+    def initialize(login)
+      @login = login
+    end
+
+    def followers()
+
+    end
+  end
+
+  class Comparer
+    def initialize(user1, user2)
+      @user1 = user1
+      @user2 = user2
+    end
+  end
+
+end
